d789868ad6ea45f18b8534d68646e0490358f69e
DESKTOP-BNOFORA\VIP <vladislawbon@yandex.ru>
2021-01-27 21:27:16 +0300

    Add Register.vue component

---
+++ clientapp/src/components/Auth/Register.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <form
+        @submit.prevent="registerUser"
+        >
+        <input
+            type="text"
+            id="firstName"
+            placeholder="firstName"
+            v-model="register.firstName"
+            required
+        />
+        <input
+            type="text"
+            id="lastName"
+            placeholder="lastName"
+            v-model="register.lastName"
+            required
+        />
+        <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            v-model="register.email"
+            required
+        />
+        <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            v-model="register.password"
+        />
+       
+        <button
+            class="btn btn-primary btn-block w-75 my-4"
+            type="submit"
+            >
+            Sign up
+        </button>
+           
+           
+        </form>
+    </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    setup(){
+        const register = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: ""
+        }
+
+       const registerUser = () => {}
+
+       return {
+           register,
+           registerUser
+       }
+    }
+})
+</script>
