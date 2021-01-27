3561b0e0571fdeb67f29d619c869e3a3f7ec5b5b
DESKTOP-BNOFORA\VIP <vladislawbon@yandex.ru>
2021-01-27 21:00:41 +0300

    Edits in the Login.vue component

--- clientapp/src/components/Auth/Login.vue
+++ clientapp/src/components/Auth/Login.vue
@@ -1,30 +1,62 @@
 <template>
     <div>
-        <h1>Login</h1>
-        Email
-        <br />
-        <input placeholder="player@example.com" v-model="email" />
-        <br /><br />
-        Password:
-        <br />
-        <input  placeholder="*********" type="password" v-model="password" />
-        <br /><br />
-        <div>
-            <button  @click="login()">LOGIN</button>
-        </div>
-        <br><br>
-    </div>  
+        <form
+            ref="formRef"
+            @submit.prevent="login"
+            >
+              <input
+                v-model="form.email"
+                class="form-control form-control-lg"
+                type="email"
+                required
+                placeholder="Email"
+              >
+              <input
+                v-model="form.password"
+                class="form-control form-control-lg"
+                type="password"
+                required
+                placeholder="Password"
+              >
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="!form.email || !form.password"
+              type="submit"
+            >
+              Sign in
+            </button>
+        </form>
+          </div>  
 </template>
 
 <script lang="ts">
-import { store } from '@/store'
+import router from "@/router";
+import { defineComponent, reactive, ref } from "vue";
 
-export default {
-    name:'Login',
-    methods:{
-        login  () {
-            store.dispatch('setLoggedIn', true)
+export default defineComponent({
+    name: 'Login',
+
+    setup(){
+        const formRef = ref<HTMLFormElement|null>(null)
+        const form = reactive({
+            email:'',
+            password:'',
+        })
+
+        const login = () => {
+            if (!formRef.value?.checkValidity()) return
+
+            const result = true; //здесь должна содержаться логика для проверки аутентификации пользователя
+            if(result){
+                router.push('/user')
+            } else return
+        }
+
+        return {
+            formRef,
+            form,
+            login
         }
     }
-}
+}) 
 </script>
