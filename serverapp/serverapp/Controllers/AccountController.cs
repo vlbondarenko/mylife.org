544500f39bd4744e772f38b27e7c315164961d49
DESKTOP-BNOFORA\VIP <vladislawbon@yandex.ru>
2021-01-31 21:47:28 +0300

    Add account controlelr

---
+++ serverapp/serverapp/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using serverapp.Infrastructure;
+using serverapp.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace serverapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // POST api/<AccountController>
+        [HttpPost]
+        public async Task<UserData> Register([FromBody] RegisterData registerData)
+        {
+            return await _userService.Register(registerData);
+        }
+
+    }
+}
