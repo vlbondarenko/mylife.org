107c2f791e5ab2c48f02beec0d74994f1bf31910
DESKTOP-BNOFORA\VIP <vladislawbon@yandex.ru>
2021-03-20 16:42:18 +0300

    Web API: I / O layer
    * Added a base controller class with a property for
    accessing a mediator instance to run commands
     from the infrastructure and the application core.
    The CQRS approach is used to separate the flow
    of creating and retrieving data from the database.
    The mediator pattern and the MediatR library are
    used to loosen the connections between parts of
    the application.
    
    * Added api controller class with user registration action
    
    * Added a middleware class for handling exceptions that
    occur while the application is running.
    
    * In the startup class, all the necessary parts of the
    application are connected (the authorization and authentication
    module, the data access module, services and the mediator
    configuration), the request pipeline is configured, and the
    exception handling middleware is connected

---
+++ serverapp/WebApi/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MediatR;
+using Infrastructure.Identity.Commands;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ApiControllerBase
+    {
+        [HttpPost]
+        [Route("registration")]
+        public async Task<IActionResult> Registration ([FromBody] CreateUserCommand command)
+        {
+            await Mediator.Send(command);
+            return Ok();
+        }
+    }
+}
---
+++ serverapp/WebApi/Helpers/CorsDependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+namespace WebApi
+{
+    public static class CorsDependencyInjection
+    {
+        private static readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public static IServiceCollection AddCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:8080", "http://localhost:8081")
+                                             .AllowAnyHeader()
+                                             .AllowAnyMethod()
+                                             .AllowCredentials();
+                                  });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseCors(this IApplicationBuilder app)
+        {
+            app.UseCors(MyAllowSpecificOrigins);
+
+            return app;
+        }
+    }
+}
