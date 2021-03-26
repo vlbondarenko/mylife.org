58d7da5b0654dff50a2eec90d3d435533f4d544d
DESKTOP-BNOFORA\VIP <vladislawbon@yandex.ru>
2021-03-26 23:21:58 +0300

    Feature: added the Application project
    * The ApplicationCore project has been replaced by the Application project.
    It will only contain user cases for the time being
    
    * added a user case for creating a user profile

---
+++ serverapp/Application/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication (this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(DependencyInjection).Assembly);
+
+            return services;
+        }
+    }
+}
---
+++ serverapp/Application/UseCases/UserProfile/Commands/CreateUserProfileCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using MediatR;
+
+using Persistence.Interfaces;
+using Domain.Entities;
+
+namespace Application.UseCases.UserProfiles.Commands
+{
+    public class CreateUserProfileCommand:IRequest
+    {
+        public string Id { get; set; }
+
+        public class Handler : IRequestHandler<CreateUserProfileCommand>
+        {
+            private readonly IApplicationDbContext _applicationDbContext;
+
+            public Handler (IApplicationDbContext applicationDbContext)
+            {
+                _applicationDbContext = applicationDbContext;
+            }
+
+            public async Task<Unit> Handle (CreateUserProfileCommand request, CancellationToken cancellationToken)
+            {
+                var userProfile = new UserProfile() 
+                { 
+                    Id = request.Id 
+                };
+
+                await _applicationDbContext.UserProfiles.AddAsync(userProfile, cancellationToken);
+                await _applicationDbContext.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            }
+        }
+    }
+}
