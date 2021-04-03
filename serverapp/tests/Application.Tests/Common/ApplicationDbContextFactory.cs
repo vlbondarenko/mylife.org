2cc0f189aff3814cd62c6de3e95fa92c0cbef062
DESKTOP-BNOFORA\VIP <vladislawbon@yandex.ru>
2021-04-03 10:13:36 +0300

    Tests, refactoring:
    * changed the abstract ApplicationDbContextFactory. Now, when creating a database,
    a single AppUser test object with test data is entered into it. Added the Destroy method
    that deletes the database after passing the tests
    
    * added a base class for testing command handlers and queries. It has instance fields
    of types  ApplicationDbContext and IMapper. Instances of the database context and mapper
    are needed for tests. The Dispose () method is also defined, which calls the Destroy() method
    of the abstract factory to delete the database after passing the tests.

---
+++ serverapp/tests/Application.Tests/Common/ApplicationDbContextFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using Persistence.Context;
+using Persistence.Interfaces;
+using Domain.Entities;
+
+namespace Application.Tests.Common
+{
+    public static class ApplicationDbContextFactory
+    {
+
+        public static ApplicationDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new ApplicationDbContext(options);
+
+            context.Database.EnsureCreated();
+            try
+            {
+                context.UserProfiles.Add(new UserProfile 
+                { 
+                    Id = "00000000-0000-0000-0000-000000000000", 
+                    FirstName = "Lol", 
+                    LastName = "Kek" ,
+                    BirthDate = DateTime.Now,
+                    City = "Sinyavka",
+                    Country = "Russia"
+                });
+                context.SaveChanges();
+            }
+            catch
+            {
+                //TODO:add logging
+            }
+          
+            return context;
+        }
+
+        public static void Destroy(ApplicationDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+        
+    }
+}
---
+++ serverapp/tests/Application.Tests/Common/ApplicationHandlerTestsBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistence.Context;
+using AutoMapper;
+using Application.Common.Mapping;
+using Xunit;
+
+namespace Application.Tests.Common
+{
+    public class ApplicationHandlerTestsBase:IDisposable
+    {
+        protected readonly ApplicationDbContext _applicationDbContext;
+        protected readonly IMapper _mapper;
+
+        public ApplicationHandlerTestsBase()
+        {
+            _applicationDbContext = ApplicationDbContextFactory.Create();
+
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ApplicationMappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            ApplicationDbContextFactory.Destroy(_applicationDbContext);
+        }
+    }
+}
